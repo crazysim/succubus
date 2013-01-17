1cfb9ac0943db4a8da771362dcf5c1210506e8fa
Nelson Chen <crazysim@gmail.com>
2013-01-17 12:39:20 -0800

    remove extraneous spacing

--- app/views/TALPodcast.scala
+++ app/views/TALPodcast.scala
@@ -18,10 +18,7 @@ object TALPodcast {
           <channel>
             <title>This American Life</title>
             <link>http://www.thisamericanlife.org</link>
-            <description>Official free, weekly podcast of the award-winning radio show "This American Life." First-person
-              stories and short fiction pieces that are touching, funny, and surprising. Hosted by Ira Glass, from WBEZ
-              Chicago Public Radio. In mp3 and updated Mondays.
-            </description>
+            <description>Official free, weekly podcast of the award-winning radio show "This American Life." First-person stories and short fiction pieces that are touching, funny, and surprising. Hosted by Ira Glass, from WBEZ Chicago Public Radio. In mp3 and updated Mondays.</description>
             <language>en</language>
             <media:thumbnail url="http://www.thisamericanlife.org/sites/all/play_music/player-logo.png"/>
             <media:category scheme="http://www.itunes.com/dtds/podcast-1.0.dtd">Society
@@ -34,15 +31,7 @@ object TALPodcast {
             <itunes:author>Chicago Public Media</itunes:author>
             <itunes:explicit>no</itunes:explicit>
             <itunes:image href="http://www.thisamericanlife.org/sites/all/play_music/player-logo.png"/>
-            <itunes:subtitle>Official free, weekly podcast of the award-winning radio show "This American Life."
-              First-person stories and short fiction pieces that are touching, funny, and surprising. Hosted by Ira Glass,
-              from WBEZ Chicago Public Media, and distributed by Public Radi
-            </itunes:subtitle>
-            <itunes:summary>Official free, weekly podcast of the award-winning radio show "This American Life." First-person
-              stories and short fiction pieces that are touching, funny, and surprising. Hosted by Ira Glass, from WBEZ
-              Chicago Public Media, and distributed by Public Radio International. In mp3 and updated Mondays.
-            </itunes:summary>
-            <itunes:category text="Society &amp; Culture"/>
+            <itunes:summary>Official free, weekly podcast of the award-winning radio show "This American Life." First-person stories and short fiction pieces that are touching, funny, and surprising. Hosted by Ira Glass, from WBEZ Chicago Public Media, and distributed by Public Radio International. In mp3 and updated Mondays.</itunes:summary>            <itunes:category text="Society &amp; Culture"/>
             <itunes:category text="Arts"/>
             <itunes:category text="News &amp; Politics"/>
             {for (episode <- tal_json.episodes) yield
