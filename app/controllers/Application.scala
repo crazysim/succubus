6bdcae3672c454fb4336912a72c6aee3fe4b87b3
Nelson Chen <crazysim@gmail.com>
2013-01-18 11:06:41 -0800

    rename test podcast

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,9 +12,8 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def podcast = Action{
+  def test_podcast = Action{
     val json_str = Source.fromURL(getClass.getResource("/test/pretty.json")).mkString
     Ok(TALPodcast(TALJSON(json_str)).rss)
   }
-  
 }
